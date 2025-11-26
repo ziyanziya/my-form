bf051854a6fb8ce0a34f41319235aa15cb23b0c4
ziyanziya <82712655+ziyanziya@users.noreply.github.com>
2025-11-26 23:16:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 document.getElementById("myForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const form = new FormData(this);
+    const data = {
+        fullname: this.fullname.value,
+        birthdate: this.birthdate.value,
+        school: this.school.value,
+        category: this.category.value
+    };
 
     fetch("https://script.google.com/macros/s/AKfycbxiJbH0cF9xsbIi116kCVqOu9TBi3QI25Nj8a-_-TvknSGi-if5SJoB__fPWMhjM6eW/exec", {
         method: "POST",
-        body: form
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
     })
-    .then(res => res.text())
+    .then(res => res.json())
     .then(data => {
         alert("✔ تم إرسال البيانات بنجاح");
         this.reset();
     })
     .catch(err => {
-        alert("❌ فشل الإرسال: تأكد من الرابط");
+        alert("❌ فشل الإرسال");
         console.error(err);
     });
 });
-
-
